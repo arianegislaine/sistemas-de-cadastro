c653c031e29d29c6ecfab1b1c8197edac74d4610
Ariane Gislaine <36931659+arianegislaine@users.noreply.github.com>
2023-01-09 22:38:10 -0300

    atualizando o init do projeto

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-consele.log("Rodando o sistema de gerenciadmento de jogos")
+console.log("Rodando o sistema de gerenciadmento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
