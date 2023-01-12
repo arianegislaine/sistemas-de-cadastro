a64fc2bdf435b0c6296d87685ece9cfcaeb944df
Ariane Gislaine <36931659+arianegislaine@users.noreply.github.com>
2023-01-11 22:25:00 -0300

    console log indicando modificações de Ariane

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciadmento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+//mais um console log de teste:
+consele.log("O Paulo fez esse commit sem você estar na aula")
+
